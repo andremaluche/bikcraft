724f31a90252d2d786f0a0b4db3b53c848478094
André Maluche <andremaluche@hotmail.com>
2022-09-07 16:00:00 -0300

    javascript-galeria

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ links.forEach(ativarLink);
 
 const parametros = new URLSearchParams(location.search);
 
-function ativarProduto (parametro) {
+function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
@@ -25,21 +25,38 @@ function ativarProduto (parametro) {
 parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
-
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls');
+  const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
 
-  resposta.classList.toggle('ativa');
+  resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
   pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
